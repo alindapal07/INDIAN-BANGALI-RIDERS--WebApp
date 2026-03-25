e9940014dc0423f69870f83324408be3b1254245
Alinda Pal <151637518+alindapal07@users.noreply.github.com>
2026-03-25 13:40:07 +0530

    Update sendEmail.js
    Fix the emailSending problem.

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,9 +1,8 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
-  port: process.env.SMTP_PORT || 465,
+  port: process.env.SMTP_PORT || "465",
   secure: true,
   auth: {
     user: process.env.SMTP_USER || 'indian.bangali.riders.2025@gmail.com',
